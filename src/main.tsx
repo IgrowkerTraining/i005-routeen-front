81929662fa0cf06d6cfd8c7511bf7a1a00b8d272
Alejandrette <alejandrette5117@gmail.com>
2025-04-02 12:54:43 +0200

    create router and layout, change App for AppRouter in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AppProvider } from './store/AppContext.tsx'
-import App from './App.tsx'
 import './index.css'
+import { AppRouter } from './router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <AppRouter />
     </AppProvider>
   </React.StrictMode>
 )
