821209cc98c5143937497d2e33b36f1bcb0f4d83
Debi312 <deboragarciaescudero@gmail.com>
2025-04-04 10:31:08 +0200

    feat:add Button reusable component; style:change logo scale to improve responsiveness; refactor: minor chanches in login form

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+
+type ButtonProps = {
+    submit?: boolean
+    text: string
+    variant?: "primary" | "secondary"
+    icon?: React.ReactNode
+    onClick?: () => void
+    className?: string
+    href?: string
+
+}
+export const Button = ({
+    submit = false,
+    text,
+    variant = "primary",
+    icon,
+    onClick,
+    href,
+    className = "",
+}: ButtonProps) => {
+    const buttonVariants = {
+        primary: "bg-primary-400 text-notwhite-400 px-5 py-2.5 rounded-md shadow-md shadow-gray-400 "
+        ,
+        secondary: "bg-secondary-400 text-notwhite-400 w-[50%] px-5 py-2.5 rounded-md shadow-md shadow-gray-400 ",
+    }
+    const content = (
+        <span
+            className={`rounded-lg px-4 py-2 flex items-center justify-center ${buttonVariants[variant]} ${className}`}
+        >
+            {icon && <span className="mr-2">{icon}</span>}
+            {text}
+        </span>
+    );
+
+    return href ? (
+        <Link to={href} className={className}>
+            {content}
+        </Link>
+    ) : (
+        <button onClick={onClick} type={submit ? 'submit' : 'button'} className="inline-block">{content}</button>
+    );
+}
+
--- src/views/Login/LoginTrainer.tsx
+++ src/views/Login/LoginTrainer.tsx
@@ -1,41 +1,62 @@
 import LoginLayout from "./LoginLayout"
 import { Link } from "react-router-dom"
 import Input from "../../components/Input/Input"
+import { Button } from "../../components/Button/Button"
+import { useNavigate } from "react-router-dom"
+import {useState} from "react"
 
 export default function LoginTrainer() {
+    const navigate = useNavigate()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleLogin = (e: React.FormEvent) => {
+        e.preventDefault()
+        console.log("Loggin in...")
+
+        setTimeout(() => {
+            navigate("/home")
+        }, 500)
+
+    }
     return (
         <LoginLayout
-        className="mt-22">
-            <div className="flex flex-col items-center gap-3">
+            className="">
+            <form onSubmit={handleLogin} className="flex flex-col items-center gap-3 ">
                 <div className="flex flex-col items-center w-[370px]">
-                   
+
                     <Input
                         id="email"
                         type="email"
                         placeholder="Ingrese el email"
-                        
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                     {/* Hay que crear un custom input para el password*/}
-        
+
                     <Input
                         id="password"
                         type="password"
                         className="mt-4"
                         placeholder="Ingrese la contraseña"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
-                <button
-                    className="bg-primary-400 w-full text-notwhite-400 px-5 py-2.5 rounded-md mt-4 shadow-md shadow-gray-400 "
-                >
-                    Iniciar sessión
-                </button>
-                <Link to="/register"
-                    className="text-primary-400 font-[600] underline underline-offset-1"
-                >
-                    Registrarse
-                </Link>
 
-            </div>
+                <div className="flex flex-col gap-4 w-full">
+                    <Button
+                        submit
+                        text="Iniciar sesión"
+                        variant="primary"
+                    />
+                    <Link to="/register"
+                        className="text-primary-400 font-[600] underline underline-offset-1"
+                    >
+                        Registrarse
+                    </Link>
+                </div>
+            </form>
         </LoginLayout>
     )
 
