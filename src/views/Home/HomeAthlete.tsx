78ffa45d9c6e75f1fa72e4aa802dbd5ff7d1ade7
Debi312 <deboragarciaescudero@gmail.com>
2025-04-07 16:19:09 +0200

    feat:implement athlete home view with initial styles; add:mock data for routines and exercices, and images

--- src/views/Home/HomeAthlete.tsx
+++ src/views/Home/HomeAthlete.tsx
@@ -1,4 +1,38 @@
-export default function HomeAthlete() {
+import { Button } from "../../components/Button/Button";
+import ExerciceCard from "../../components/cards/ExerciceCard/ExerciceCard";
+import { Link } from "react-router-dom";
 
-    return <div>Hello, Athlete!</div>;
+export default function HomeAthlete() {
+    return (
+        <main className="flex flex-col justify-between h-screen px-4 pb-2 gap-6">
+
+            <section className="flex flex-col justify-start w-full gap-5">
+
+                <h2 className="text-[30px] text-notblack-400 ">
+                    Te damos la bienvenida
+                </h2>
+                <div className="flex justify-start items-center gap-5 w-full ">
+                    <Link to="/routine"
+                        className="text-primary-400 font-[600] underline  underline-offset-2"
+                    >
+                        Plan de hoy
+                    </Link>
+                    <Link to="/progress"
+                        className="text-primary-400 font-[600] underline underline-offset-2"
+                    >
+                        Progreso
+                    </Link>
+                </div>
+                <ExerciceCard />
+            </section>
+            <Button
+                text="Agregar"
+                variant="primary"
+                onClick={() => console.log("Agregar Alumno")}
+                icon={<i className="bi bi-person-fill-add "></i>
+                }
+            />
+        </main>
+
+    )
 }
