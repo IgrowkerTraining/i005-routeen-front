dbbabf9e2997835b15cb49fafe4b9d3b76e74814
AgusBirman <agusbirman@gmail.com>
2025-04-09 17:29:11 +0200

    changed local storage to cookies, and added getTokenData

--- src/logic/auth/authAthlete.ts
+++ src/logic/auth/authAthlete.ts
@@ -2,8 +2,11 @@ import axios from '../../api/axiosInstance'
 import { AuthAthleteInput, AuthTrainerResponse } from '../interfaces/auth'
 
 const authAthlete = async (data: AuthAthleteInput): Promise<AuthTrainerResponse> => {
-    const res = await axios.post<AuthTrainerResponse>('/auth/athlete', data)
-    return res.data
+    const res = await axios.post<AuthTrainerResponse>('/auth/athlete', data, {
+        withCredentials: true,
+    });
+
+    return res.data;
 }
 
 export default authAthlete
