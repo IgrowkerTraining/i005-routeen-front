95c3338489217162b6421742ce40eaa0fbc1f48d
Debi312 <deboragarciaescudero@gmail.com>
2025-04-20 16:55:40 +0200

    refactor: separate RoutineCard concerns and simplify typings by adding another RoutineAssignedCard component for displaying assigned routines

--- src/logic/interfaces/trainer.ts
+++ src/logic/interfaces/trainer.ts
@@ -22,3 +22,19 @@ export interface RegisterTrainerInput {
     phone: string
     date_birth: string
 }
+export interface RoutineAssigned {
+    routine_id: Routine
+    athlete_id: string
+    description: string
+    assignment_date: string
+    id: string
+    name: string;
+}
+    
+export interface Routine{
+    _id: string;
+    name: string;
+    description: string;
+    trainer_id: string;
+    id: string;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -29,3 +29,4 @@ export type ToastMessage = {
   message: string
   isVisible: boolean 
 }
+
